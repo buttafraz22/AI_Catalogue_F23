7d8d55b7784fa4609dfc3fb3cff1a559d9724808
buttafraz22 <109384539+buttafraz22@users.noreply.github.com>
2023-12-06 21:52:57 +0500

    Labs Added

---
+++ Labs/Week 1/AI_Lab_01.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # <center>Artificial Intelligence</center>  <center>Fall 2023</center>
+# ## Lab 01
+# #### Name:
+# #### Roll number:
+# 
+
+# Take weight in kgs and convert it into pounds. 1 pound = 1 kg 2.2046 **(2 marks)**
+
+# In[1]:
+
+
+## add code here
+
+
+# Calculate the cost of all the items in a shopping cart. **(2 marks)**
+
+# In[2]:
+
+
+prices =[20,50,80,10,56,89]
+
+## add code here
+
+
+# Write a function that returns the maximum of two numbers. **(2 marks)**
+
+# In[3]:
+
+
+## add code here
+
+
+# Write a function called **deepmind** that takes a number  **(4 marks)**
+# * If the number is divisible by 3, it should return deep.
+# * If it is divisible by 5, it should return mind.
+# * If it is divisible by both 3 and 5, it should return deepmind.
+# * Otherwise, it should return the same number.
+# 
+# 
+# 
+
+# In[4]:
+
+
+## add code here
+
+
+# **listA** =  [1,2,3,4,5,6,7,8,9,10]  
+# 
+# If an element of **listA** is smaller than 5, replace it with 0. And if an element of x is bigger than 5, replace it with 1. (**2 marks**)
+
+# In[5]:
+
+
+## add code here
+
+
+# Compute the square of **listA** elements in one line. (**2 marks**)
+# 
+
+# In[6]:
+
+
+listA = [1,2,3,4,5,6,7,8,9,10]
+## add code here
+
+
+# Concatenate b1 and b2. (**2 marks**)
+
+# In[7]:
+
+
+b1 = ['Hello', 'in','first']
+b2 = ['Students','the','recitation']
+## add code here
+
+
+# Create a dictionary of student **Ali** where the keys are courses and values are total and obtaining marks in each course. Print the dictionary items subjects wise **(2 marks)**
+
+# In[8]:
+
+
+## add code here
+
+
+# Create a class 'calculator' with the following functions to compute i) addition, ii) subtraction, iii)multiplication, iv)division and v)square
+# between two numbers. **(2 marks)**
+
+# In[9]:
+
+
+## add code here
+
